9a2a0f67f6678c407ad121b5b7525807df1ad332
Daniel Merritt <daniel.mafou@gmail.com>
2023-10-23 18:37:26 -0400

    made board

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+// cached elements
+const gameBoard = document.querySelector(".game-board")
+
+console.log(gameBoard)
+
+// constants
+let difficulty = ['easy', 'medium', 'hard']
+let currDifficulty = difficulty[0]
+
+// functions
+
+function init() {
+  if (currDifficulty == 'easy') {
+    const mines = 10
+    const width = 8
+    for (let i=0; i < width*width; i++) {
+      let square = document.createElement("div")
+      // console.log(square)
+      gameBoard.append(square)
+    }
+  } else if (currDifficulty == 'medium') {
+    console.log(currDifficulty)
+  } else {
+    console.log(currDifficulty)
+  }
+}
+
+init()
